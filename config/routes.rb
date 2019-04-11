d47621ede30a84306e8148aa79eb3156e56b0ce1
itsuki shun <shun@air.local>
2019-04-11 21:14:39 +0900

    [finish]2-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+ resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'books#top'
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'book/:id' => 'books#show', as: 'book'
 end
